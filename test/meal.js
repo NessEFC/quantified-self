91e30292cb1832ed3dd2461f94f9407bb40ee652
YOUR NAME <ebassity@gmail.com>
2017-07-12 16:47:10 -0600

    Adds CSS Grid layout and form styling

---
+++ test/meal.js
@@ -0,0 +1,25 @@
+// const Meal = require("../lib/meal")
+//
+// describe('Meal', function() {
+//   it('can turn an api response into HTML', function(done) {
+//     var APIresponse = new Promise(function(resolve, reject){
+//       resolve({}
+//         name: "Breakfast",
+//       goal_calories: 400,
+//       foods: [
+//         {
+//           name: "Banana",
+//           calories: 35
+//         }
+//       ]
+//     })
+//       if(false)
+//       reject("nope")
+//     })
+//     Meal.fromAPIToHTML(APIResponse)
+//     .then(function(mealHTML){
+//       assert.include("Banana". mealHTML)
+//     })
+//     done()
+//   })
+// })
